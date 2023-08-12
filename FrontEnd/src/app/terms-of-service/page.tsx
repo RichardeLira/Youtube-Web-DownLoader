d468cd583a5cbc50f73262ea00966388803dc389
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-08-12 14:19:18 -0300

    terms of service and privacy policy completed

--- FrontEnd/src/app/terms-of-service/page.tsx
+++ FrontEnd/src/app/terms-of-service/page.tsx
@@ -1,3 +1,162 @@
 export default function TermsOfService() {
-  return <div>Terms of service</div>
+  return (
+    <div className="flex w-full justify-center py-16">
+      <div className="w-full max-w-[1440px] px-16">
+        <h1 className="mb-4 text-xl font-bold sm:text-4xl">
+          Termos de Serviço
+        </h1>
+        <h2 className="mb-4 font-semibold">
+          <span>1. Termos</span>
+        </h2>
+        <p className="mb-4">
+          <span>
+            Ao acessar ao site
+            <a href="m2youtube.com">m2youtube</a>, concorda em cumprir estes
+            termos de serviço, todas as leis e regulamentos aplicáveis e
+            concorda que é responsável pelo cumprimento de todas as leis locais
+            aplicáveis. Se você não concordar com algum desses termos, está
+            proibido de usar ou acessar este site. Os materiais contidos neste
+            site são protegidos pelas leis de direitos autorais e marcas
+            comerciais aplicáveis.
+          </span>
+        </p>
+        <h2 className="mb-4 font-semibold">
+          <span>2. Uso de Licença</span>
+        </h2>
+        <p className="mb-4">
+          <span>
+            É concedida permissão para baixar temporariamente uma cópia dos
+            materiais (informações ou software) no site m2youtube , apenas para
+            visualização transitória pessoal e não comercial. Esta é a concessão
+            de uma licença, não uma transferência de título e, sob esta licença,
+            você não pode:&nbsp;
+          </span>
+        </p>
+        <ol className="mb-4 list-decimal pl-8">
+          <li>
+            <span>modificar ou copiar os materiais;</span>
+          </li>
+          <li>
+            <span>
+              usar os materiais para qualquer finalidade comercial ou para
+              exibição pública (comercial ou não comercial);
+            </span>
+          </li>
+          <li>
+            <span>
+              tentar descompilar ou fazer engenharia reversa de qualquer
+              software contido no site m2youtube;
+            </span>
+          </li>
+          <li>
+            <span>
+              remover quaisquer direitos autorais ou outras notações de
+              propriedade dos materiais; ou
+            </span>
+          </li>
+          <li>
+            <span>
+              transferir os materiais para outra pessoa ou &quot;espelhe&quot;
+              os materiais em qualquer outro servidor.
+            </span>
+          </li>
+        </ol>
+        <p className="mb-4">
+          <span>
+            Esta licença será automaticamente rescindida se você violar alguma
+            dessas restrições e poderá ser rescindida por m2youtube a qualquer
+            momento. Ao encerrar a visualização desses materiais ou após o
+            término desta licença, você deve apagar todos os materiais baixados
+            em sua posse, seja em formato eletrónico ou impresso.
+          </span>
+        </p>
+        <h2 className="mb-4 font-semibold">
+          <span>3. Isenção de responsabilidade</span>
+        </h2>
+        <ol className="mb-4 list-decimal pl-8">
+          <li>
+            <span>
+              Os materiais no site da m2youtube são fornecidos &quot;como
+              estão&quot;. m2youtube não oferece garantias, expressas ou
+              implícitas, e, por este meio, isenta e nega todas as outras
+              garantias, incluindo, sem limitação, garantias implícitas ou
+              condições de comercialização, adequação a um fim específico ou não
+              violação de propriedade intelectual ou outra violação de direitos.
+            </span>
+          </li>
+          <li>
+            <span>
+              Além disso, o m2youtube não garante ou faz qualquer representação
+              relativa à precisão, aos resultados prováveis ou à confiabilidade
+              do uso dos materiais em seu site ou de outra forma relacionado a
+              esses materiais ou em sites vinculados a este site.
+            </span>
+          </li>
+        </ol>
+        <h2 className="mb-4 font-semibold">
+          <span>4. Limitações</span>
+        </h2>
+        <p className="mb-4">
+          <span>
+            Em nenhum caso o m2youtube ou seus fornecedores serão responsáveis
+            por quaisquer danos (incluindo, sem limitação, danos por perda de
+            dados ou lucro ou devido a interrupção dos negócios) decorrentes do
+            uso ou da incapacidade de usar os materiais em m2youtube, mesmo que
+            m2youtube ou um representante autorizado da m2youtube tenha sido
+            notificado oralmente ou por escrito da possibilidade de tais danos.
+            Como algumas jurisdições não permitem limitações em garantias
+            implícitas, ou limitações de responsabilidade por danos conseqüentes
+            ou incidentais, essas limitações podem não se aplicar a você.
+          </span>
+        </p>
+        <h2 className="mb-4 font-semibold">
+          <span>5. Precisão dos materiais</span>
+        </h2>
+        <p className="mb-4">
+          <span>
+            Os materiais exibidos no site da m2youtube podem incluir erros
+            técnicos, tipográficos ou fotográficos. m2youtube não garante que
+            qualquer material em seu site seja preciso, completo ou atual.
+            m2youtube pode fazer alterações nos materiais contidos em seu site a
+            qualquer momento, sem aviso prévio. No entanto, m2youtube não se
+            compromete a atualizar os materiais.
+          </span>
+        </p>
+        <h2 className="mb-4 font-semibold">
+          <span>6. Links</span>
+        </h2>
+        <p className="mb-4">
+          <span>
+            O m2youtube não analisou todos os sites vinculados ao seu site e não
+            é responsável pelo conteúdo de nenhum site vinculado. A inclusão de
+            qualquer link não implica endosso por m2youtube do site. O uso de
+            qualquer site vinculado é por conta e risco do usuário.
+          </span>
+        </p>
+        <p>
+          <br />
+        </p>
+        <h3 className="mb-4 font-semibold">
+          <span>Modificações</span>
+        </h3>
+        <p className="mb-4">
+          <span>
+            O m2youtube pode revisar estes termos de serviço do site a qualquer
+            momento, sem aviso prévio. Ao usar este site, você concorda em ficar
+            vinculado à versão atual desses termos de serviço.
+          </span>
+        </p>
+        <h3 className="mb-4 font-semibold">
+          <span>Lei aplicável</span>
+        </h3>
+        <p className="mb-4">
+          <span>
+            Estes termos e condições são regidos e interpretados de acordo com
+            as leis do m2youtube e você se submete irrevogavelmente à jurisdição
+            exclusiva dos tribunais naquele estado ou localidade.
+          </span>
+        </p>
+      </div>
+    </div>
+  )
 }
